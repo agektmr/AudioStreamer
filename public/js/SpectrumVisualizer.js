6d79945c6850acc4b27bcd10ac8ffdf69813b22a
Eiji Kitamura <agektmr@gmail.com>
2012-03-02 23:09:57 +0900

    refactored and optimized SpectrumVisualizer

--- public/js/SpectrumVisualizer.js
+++ public/js/SpectrumVisualizer.js
@@ -1,48 +1,29 @@
-/*
- * params {
- *   elem
- *   width
- *   height
- *   smoothingTimeConstant
- * }
- */
-var SpectrumVisualizer = function(audioContext, params) {
-  this.ac = audioContext;
-  if (params.elem.querySelector('canvas')) {
-    var canvas = params.elem.querySelector('canvas');
-  } else {
-    var canvas = document.createElement('canvas');
-    params.elem.appendChild(canvas);
-  }
-  canvas.setAttribute('width', params.width || 400);
-  canvas.setAttribute('height', params.height || 200);
-  this.cc = canvas.getContext('2d');
-
-  this.width = parseInt(canvas.width);
-  this.height = parseInt(canvas.height);
-
-  this.analyser = this.ac.createAnalyser();
-  this.analyser.smoothingTimeConstant = params.smoothingTimeConstant || 0.3;
+var SpectrumVisualizer = (function() {
+  var visualizers = [],
+      requestAnimationFrame = window.requestAnimationFrame ||
+                              window.webkitRequestAnimationFrame ||
+                              window.mozRequestAnimationFrame;
   var analyse = function() {
-    var freqByteData = new Uint8Array(this.analyser.frequencyBinCount);
-    this.analyser.getByteFrequencyData(freqByteData);
-    this._draw(freqByteData);
-    webkitRequestAnimationFrame(analyse.bind(this));
+    for (var i = 0; i < visualizers.length; i++) {
+      // remove visualizer if deleted without disconnection
+      if (!visualizers[i].analyser) {
+        visualizers[i--].disconnect();
+        continue;
+      }
+      var freqByteData = new Uint8Array(visualizers[i].analyser.frequencyBinCount);
+      visualizers[i].analyser.getByteFrequencyData(freqByteData);
+      draw.call(visualizers[i], freqByteData);
+    }
+    requestAnimationFrame(analyse);
   };
-  analyse.call(this);
-};
-SpectrumVisualizer.prototype = {
-  connect: function(source, destination) {
-    source.connect(this.analyser);
-    this.analyser.connect(destination);
-  },
-  disconnect: function() {
-    if (this.source) this.source.disconnect();
-    this.analyser.disconnect();
-  },
-  _draw: function(freq) {
+
+  var clear = function() {
+    this.cc.clearRect(0, 0, this.width, this.height);
+  };
+
+  var draw = function(freq) {
     var length = freq.length;
-    this._clear();
+    clear.call(this);
 
     // Draw rectangle for each frequency bin.
     this.cc.beginPath();
@@ -53,8 +34,55 @@ SpectrumVisualizer.prototype = {
       this.cc.lineTo(i + 1, value);
     }
     this.cc.stroke();
-  },
-  _clear: function() {
-    this.cc.clearRect(0, 0, this.width, this.height);
+  };
+
+  /*
+   * params {
+   *   elem
+   *   width
+   *   height
+   *   smoothingTimeConstant
+   * }
+   */
+  var SpectrumVisualizer = function(audioContext, params) {
+    this.ac = audioContext;
+    if (params.elem.querySelector('canvas')) {
+      var canvas = params.elem.querySelector('canvas');
+    } else {
+      var canvas = document.createElement('canvas');
+      params.elem.appendChild(canvas);
+    }
+    canvas.setAttribute('width', params.width || 400);
+    canvas.setAttribute('height', params.height || 200);
+    this.cc = canvas.getContext('2d');
+
+    this.width = parseInt(canvas.width);
+    this.height = parseInt(canvas.height);
+
+    this.analyser = this.ac.createAnalyser();
+    this.analyser.smoothingTimeConstant = params.smoothingTimeConstant || 0.3;
+  };
+  SpectrumVisualizer.prototype = {
+    connect: function(source, destination) {
+      visualizers.push(this);
+      source.connect(this.analyser);
+      this.analyser.connect(destination);
+    },
+    disconnect: function() {
+      if (this.source) this.source.disconnect();
+      this.analyser.disconnect();
+      for (var i = 0; visualizers.length; i++) {
+        if (visualizers[i] == this) {
+          visualizers.splice(i);
+        }
+      }
+    }
+  };
+
+  // start analyse interruption
+  analyse();
+
+  return function(audioContext, params) {
+    return new SpectrumVisualizer(audioContext, params);
   }
-};
+})();
